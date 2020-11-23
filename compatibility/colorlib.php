63ce88edc7afca9f77c3572bfadc69df754f3682
Danny Cooper <info@dannycooper.org>
2020-11-23 15:10:44 +0000

    Add compatibility pack for ColorLib themes

---
+++ compatibility/colorlib.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Compatibility file for ColorLib themes.
+ *
+ * @package   olympus-google-fonts
+ * @copyright Copyright (c) 2020, Fonts Plugin
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+
+/**
+ * Modify the default element selectors to improve compatibility with ColorLib themes.
+ *
+ * @param array $elements The default elements.
+ */
+function ogf_colorlib_elements( $elements ) {
+
+	$elements['ogf_inputs']['selectors'] = 'button, .button, input, select, textarea, .wp-block-button, .wp-block-button__link, .btn, .header-button-one, .header-button-two, .latest-news-button, .readmore, .submit, #submit, .more-link, .blog-post-button';
+	return $elements;
+
+}
+
+add_filter( 'ogf_elements', 'ogf_colorlib_elements' );
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Donate link: https://fontsplugin.com/#pricing
 Requires at least: 4.0
 Tested up to: 5.5
 License: GPLv2 or later
-Stable tag: 2.3.6
+Stable tag: 2.3.7
 
 The easiest to use Google Fonts Typography Plugin. No coding required. 900+ font choices.
 
