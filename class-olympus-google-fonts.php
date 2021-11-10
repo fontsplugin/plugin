92e56378ceee449189aed0ef6cf92a27802b0da3
Danny Cooper <email@dannycooper.com>
2021-11-10 22:02:48 +0000

    update fonts list

--- class-olympus-google-fonts.php
+++ class-olympus-google-fonts.php
@@ -39,7 +39,7 @@ class Olympus_Google_Fonts {
 	 */
 	public function constants() {
 		if ( ! defined( 'OGF_VERSION' ) ) {
-			define( 'OGF_VERSION', '3.0.6' );
+			define( 'OGF_VERSION', '3.0.7' );
 		}
 
 		if ( ! defined( 'OGF_DIR_PATH' ) ) {
