6cc08fe03a076524447567b959b4c9cb11b5eff7
Danny Cooper <email@dannycooper.com>
2022-11-18 16:11:59 +0000

    move to pro

--- assets/js/customize-controls.js
+++ assets/js/customize-controls.js
@@ -336,16 +336,3 @@ jQuery( document ).ready( function() {
 		},
 	} );
 }( wp.customize ) );
-
-
-(function($){
-    wp.customize( 'fpp_host_locally', function( fpp_host_locally ) {
-        fpp_host_locally.bind( function( value ) {
-            if( true == value ){
-                $('#customize-control-fpp_use_woff2').show();
-            } else {
-                $('#customize-control-fpp_use_woff2').hide();
-            }
-        } );
-    } );
-})(jQuery);
