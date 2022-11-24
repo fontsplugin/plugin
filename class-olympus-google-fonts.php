30643e573eae18b18957096b6f5df2d534f5b322
Danny Cooper <email@dannycooper.com>
2022-11-24 13:42:27 +0000

    version bump

--- class-olympus-google-fonts.php
+++ class-olympus-google-fonts.php
@@ -39,7 +39,7 @@ class Olympus_Google_Fonts {
 	 */
 	public function constants() {
 		if ( ! defined( 'OGF_VERSION' ) ) {
-			define( 'OGF_VERSION', '3.2.4' );
+			define( 'OGF_VERSION', '3.2.5' );
 		}
 
 		if ( ! defined( 'OGF_DIR_PATH' ) ) {
