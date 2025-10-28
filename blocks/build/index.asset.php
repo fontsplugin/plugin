e67f4d8826b6fd518a04a5d0e2cbd9f446d7e654
Danny Cooper <email@dannycooper.com>
2025-10-28 11:31:49 +0000

    update fonts list

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7298e6461f3acd502da3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd7ba6acf239bd7049db9');
