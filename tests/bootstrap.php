581363e3c95f3825f373a5705b879083b64c7f82
Danny Cooper <email@dannycooper.com>
2026-01-13 11:36:19 +0000

    Fix bootstrap to auto-detect WordPress test suite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -36,23 +36,16 @@ class OGF_Unit_TestCase extends TestCase {
 }
 
 /**
- * Check if we're running integration tests.
+ * Check if WordPress test suite is available and load it.
  */
-$is_integration = in_array( '--testsuite=integration', $_SERVER['argv'] ?? array(), true );
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-if ( $is_integration ) {
-	// Integration tests need WordPress.
-	$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
 
-	if ( ! $_tests_dir ) {
-		$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-	}
-
-	if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
-		echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh?" . PHP_EOL;
-		exit( 1 );
-	}
-
+// If WP test suite exists, load it (makes WP_UnitTestCase available).
+if ( file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
 	// Give access to tests_add_filter() function.
 	require_once "{$_tests_dir}/includes/functions.php";
 
