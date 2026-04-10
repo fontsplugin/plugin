41df40d3459d2eab1310483e39ccad1928b4206b
Danny Cooper <email@dannycooper.com>
2026-04-10 10:54:39 +0100

    update deps

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd7ba6acf239bd7049db9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3692f696a9b414e31ada');
